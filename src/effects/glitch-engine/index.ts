172dfd0b54a9c1e69310e93e446bdd901ab3e1c6
thekevinboyle <thekevinboyle@gmail.com>
2026-01-25 20:33:20 -0600

    feat: add glitch engine module store

---
+++ src/effects/glitch-engine/index.ts
@@ -0,0 +1,8 @@
+export { RGBSplitEffect, DEFAULT_RGB_SPLIT_PARAMS } from './RGBSplitEffect'
+export type { RGBSplitParams } from './RGBSplitEffect'
+
+export { BlockDisplaceEffect, DEFAULT_BLOCK_DISPLACE_PARAMS } from './BlockDisplaceEffect'
+export type { BlockDisplaceParams } from './BlockDisplaceEffect'
+
+export { ScanLinesEffect, DEFAULT_SCAN_LINES_PARAMS } from './ScanLinesEffect'
+export type { ScanLinesParams } from './ScanLinesEffect'
---
+++ src/stores/glitchEngineStore.ts
@@ -0,0 +1,71 @@
+import { create } from 'zustand'
+import {
+  RGBSplitParams,
+  DEFAULT_RGB_SPLIT_PARAMS
+} from '../effects/glitch-engine/RGBSplitEffect'
+import {
+  BlockDisplaceParams,
+  DEFAULT_BLOCK_DISPLACE_PARAMS
+} from '../effects/glitch-engine/BlockDisplaceEffect'
+import {
+  ScanLinesParams,
+  DEFAULT_SCAN_LINES_PARAMS
+} from '../effects/glitch-engine/ScanLinesEffect'
+
+interface GlitchEngineState {
+  enabled: boolean
+
+  rgbSplitEnabled: boolean
+  blockDisplaceEnabled: boolean
+  scanLinesEnabled: boolean
+
+  rgbSplit: RGBSplitParams
+  blockDisplace: BlockDisplaceParams
+  scanLines: ScanLinesParams
+
+  setEnabled: (enabled: boolean) => void
+  setRGBSplitEnabled: (enabled: boolean) => void
+  setBlockDisplaceEnabled: (enabled: boolean) => void
+  setScanLinesEnabled: (enabled: boolean) => void
+  updateRGBSplit: (params: Partial<RGBSplitParams>) => void
+  updateBlockDisplace: (params: Partial<BlockDisplaceParams>) => void
+  updateScanLines: (params: Partial<ScanLinesParams>) => void
+  reset: () => void
+}
+
+export const useGlitchEngineStore = create<GlitchEngineState>((set) => ({
+  enabled: true,
+
+  rgbSplitEnabled: true,
+  blockDisplaceEnabled: false,
+  scanLinesEnabled: false,
+
+  rgbSplit: { ...DEFAULT_RGB_SPLIT_PARAMS },
+  blockDisplace: { ...DEFAULT_BLOCK_DISPLACE_PARAMS },
+  scanLines: { ...DEFAULT_SCAN_LINES_PARAMS },
+
+  setEnabled: (enabled) => set({ enabled }),
+  setRGBSplitEnabled: (enabled) => set({ rgbSplitEnabled: enabled }),
+  setBlockDisplaceEnabled: (enabled) => set({ blockDisplaceEnabled: enabled }),
+  setScanLinesEnabled: (enabled) => set({ scanLinesEnabled: enabled }),
+
+  updateRGBSplit: (params) => set((state) => ({
+    rgbSplit: { ...state.rgbSplit, ...params }
+  })),
+  updateBlockDisplace: (params) => set((state) => ({
+    blockDisplace: { ...state.blockDisplace, ...params }
+  })),
+  updateScanLines: (params) => set((state) => ({
+    scanLines: { ...state.scanLines, ...params }
+  })),
+
+  reset: () => set({
+    enabled: true,
+    rgbSplitEnabled: true,
+    blockDisplaceEnabled: false,
+    scanLinesEnabled: false,
+    rgbSplit: { ...DEFAULT_RGB_SPLIT_PARAMS },
+    blockDisplace: { ...DEFAULT_BLOCK_DISPLACE_PARAMS },
+    scanLines: { ...DEFAULT_SCAN_LINES_PARAMS },
+  }),
+}))

68712c9a05bd7ed3fbb54a1fcebe9749b6b13896
thekevinboyle <thekevinboyle@gmail.com>
2026-01-27 14:31:43 -0600

    feat: add contour module exports

---
+++ src/effects/contour/index.ts
@@ -0,0 +1,8 @@
+export { MarchingSquares } from './MarchingSquares'
+export type { Contour, ContourPoint } from './MarchingSquares'
+
+export { ContourTracker } from './ContourTracker'
+export type { TrackedContour } from './ContourTracker'
+
+export { OrganicRenderer } from './OrganicRenderer'
+export type { RenderStyle, TrailPoint } from './OrganicRenderer'
